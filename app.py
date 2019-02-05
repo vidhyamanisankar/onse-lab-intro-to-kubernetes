2de4e673f9a14e08129975ec72c0f618200cc035
vidhyamanisankar <vidhya4.shekar@gmail.com>
2019-02-05 16:03:06 +0000

    updating app

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app = Flask(__name__, instance_relative_config=True)
 @app.route('/')
 def hello_world():
     recipient = os.getenv("RECEIVER", "unknown")
+    
 
     return jsonify({"message": "Hello", "recipient": recipient, "result": "OK"})
 
